f24abc26a65b63fb83573705fcf36c19ff1fad45
Michael Scola <mikescola@gmail.com>
2024-06-15 14:28:29 -0500

    updated model

--- src/Enums/ModelType.php
+++ src/Enums/ModelType.php
@@ -6,7 +6,7 @@ namespace Gemini\Enums;
 
 enum ModelType: string
 {
-    case GEMINI_PRO = 'models/gemini-pro';
+    case GEMINI_PRO = 'models/gemini-1.5-pro';
     case GEMINI_PRO_VISION = 'models/gemini-pro-vision';
     case EMBEDDING = 'models/embedding-001';
 }
